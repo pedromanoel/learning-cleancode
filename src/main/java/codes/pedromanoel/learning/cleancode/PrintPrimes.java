125e76810684595012772289e8e58ce9e9ed5dda
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2019-05-09 00:43:34 -0300

    Move variable declaration closer to usage

--- src/main/java/codes/pedromanoel/learning/cleancode/PrintPrimes.java
+++ src/main/java/codes/pedromanoel/learning/cleancode/PrintPrimes.java
@@ -10,29 +10,17 @@ class PrintPrimes {
 
     public void run() {
         final int M = 1000;
-        final int RR = 50;
-        final int CC = 4;
-        final int WW = 10;
         final int ORDMAX = 30;
-        int P[] = new int[M + 1];
-        int PAGENUMBER;
-        int PAGEOFFSET;
-        int ROWOFFSET;
-        int C;
 
-        int J;
-        int K;
         boolean JPRIME;
-        int ORD;
-        int SQUARE;
-        int N;
         int MULT[] = new int[ORDMAX + 1];
 
-        J = 1;
-        K = 1;
+        int J = 1;
+        int K = 1;
+        int ORD = 2;
+        int SQUARE = 9;
+        int P[] = new int[M + 1];
         P[1] = 2;
-        ORD = 2;
-        SQUARE = 9;
 
         while (K < M) {
             do {
@@ -42,7 +30,7 @@ class PrintPrimes {
                     SQUARE = P[ORD] * P[ORD];
                     MULT[ORD - 1] = J;
                 }
-                N = 2;
+                int N = 2;
                 JPRIME = true;
                 while (N < ORD && JPRIME) {
                     while (MULT[N] < J)
@@ -57,14 +45,17 @@ class PrintPrimes {
             P[K] = J;
         }
         {
-            PAGENUMBER = 1;
-            PAGEOFFSET = 1;
+            final int RR = 50;
+            final int CC = 4;
+            final int WW = 10;
+            int PAGENUMBER = 1;
+            int PAGEOFFSET = 1;
             while (PAGEOFFSET <= M) {
                 System.out.println("The First " + M +
                         " Prime Numbers --- Page " + PAGENUMBER);
                 System.out.println("");
-                for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++) {
-                    for (C = 0; C < CC; C++)
+                for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++) {
+                    for (int C = 0; C < CC; C++)
                         if (ROWOFFSET + C * RR <= M)
                             System.out.format("%10d", P[ROWOFFSET + C * RR]);
                     System.out.println("");
